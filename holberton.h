79827465c6f1a95322e835938cbab985ff893b55
miguel-dev <miguelacortes@zoho.com>
2019-03-19 20:56:54 +0000

    [feature]Working mandatory tasks

--- holberton.h
+++ holberton.h
@@ -3,11 +3,12 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 /**
  * struct print - connects the format with the parameter.
  * @format: format of the parameter.
- * @parameter: parameter to be printed.
+ * @function: pointer to the function to printed.
  *
  * Description: Structure linking each parameter with it's format.
  */
@@ -15,12 +16,16 @@
 typedef struct print
 {
 	char *format;
-	void (*f) (va_list parameter);
+	int (*function)(va_list parameter);
 } print_p;
 
 int format_c(va_list parameter);
 
 int format_s(va_list parameter);
+
+int format_i(va_list parameter);
+
+int (*select_format_func(const char *))(va_list parameter);
 
 int _printf(const char *format, ...);
 
