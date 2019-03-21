06f0a1e3d575dd015a799905e2e2b2ae9ecc602a
miguel-dev <miguelacortes@zoho.com>
2019-03-21 00:34:18 +0000

    [Fixes] Handles special cases

--- test/main.c
+++ test/main.c
@@ -3,14 +3,18 @@
 #include "holberton.h"
 
 /**
+  mplete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
  * main - Entry point
  *
  * Return: Always 0
  */
 int main(void)
 {
-	int len;
-	/*len = _printf("%s", "This sentence is retrieved from va_args!\n");
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+	/*int len;
+	len = _printf("%s", "This sentence is retrieved from va_args!\n");
+		i"s", format_s},
+		{NULL, NULL}
 	_printf("\n%i", len);*/
   /*unsigned int ui;
 	void *addr;
@@ -19,12 +23,12 @@ int main(void)
 	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
 	_printf("%%");
-	_printf("Length:[%d, %i]\n", len, len);*/
-  	len = _printf("%d\n", 1234);
+	_printf("Length:[%d, %i]\n", len, len);
+  	len = _printf("%d\n", 1234);*/
 	/*	_printf("Unsigned:[%u]\n", ui);
 	_printf("Unsigned octal:[%o]\n", ui);
-	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);*/
-	printf("\n%i\n", len);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	printf("\n%i\n", len);*/
 	/*	_printf("String:[%s]\n", "I am a string !");
 		_printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
